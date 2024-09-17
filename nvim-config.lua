92810a6f80f9ce608886687ea58647a2185cf6c8
Jinming Wu, Patrick <me@patrickwu.space>
2024-09-17 15:48:49 +0800

    neovim: add copilot chat

--- nvim-config.lua
+++ nvim-config.lua
@@ -45,6 +45,20 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	{ "github/copilot.vim" },
 	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	{
 		"nanozuki/tabby.nvim",
 		-- event = 'VimEnter', -- if you want lazy load, see below
 		dependencies = "nvim-tree/nvim-web-devicons",
