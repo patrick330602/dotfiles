1329028c8fed29337e536eacba7b928636046832
Jinming Wu, Patrick <me@patrickwu.space>
2024-09-17 15:18:12 +0800

    neovim: replace tabular with tabby and add tinygit shortcut

--- nvim-config.lua
+++ nvim-config.lua
@@ -40,7 +40,63 @@ vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
     {'github/copilot.vim'},
-    {'godlygeek/tabular'},
+	{
+		  'nanozuki/tabby.nvim',
+		    -- event = 'VimEnter', -- if you want lazy load, see below
+			dependencies = 'nvim-tree/nvim-web-devicons',
+			config = function()
+                vim.o.showtabline = 2
+                local theme = {
+                    fill = 'TabLineFill',
+                    -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+                    head = 'TabLine',
+                    current_tab = 'TabLineSel',
+                    tab = 'TabLine',
+                    win = 'TabLine',
+                    tail = 'TabLine',
+                  }
+                  require('tabby').setup({
+                    line = function(line)
+                      return {
+                        {
+                          { '  ', hl = theme.head },
+                          line.sep('', theme.head, theme.fill),
+                        },
+                        line.tabs().foreach(function(tab)
+                          local hl = tab.is_current() and theme.current_tab or theme.tab
+                          return {
+                            line.sep('', hl, theme.fill),
+                            tab.is_current() and '' or '󰆣',
+                            tab.number(),
+                            tab.name(),
+                            tab.close_btn(''),
+                            line.sep('', hl, theme.fill),
+                            hl = hl,
+                            margin = ' ',
+                          }
+                        end),
+                        line.spacer(),
+                        line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+                          return {
+                            line.sep('', theme.win, theme.fill),
+                            win.is_current() and '' or '',
+                            win.buf_name(),
+                            line.sep('', theme.win, theme.fill),
+                            hl = theme.win,
+                            margin = ' ',
+                          }
+                        end),
+                        {
+                          line.sep('', theme.tail, theme.fill),
+                          { '  ', hl = theme.tail },
+                        },
+                        hl = theme.fill,
+                      }
+                    end,
+                    -- option = {}, -- setup modules' option,
+                  })
+			end,
+	},
     {'vim-airline/vim-airline'},
     {'vim-airline/vim-airline-themes'},
     {'tpope/vim-fugitive'},
@@ -104,6 +160,8 @@ vim.api.nvim_set_keymap('n', '<A-Left>',
 vim.api.nvim_set_keymap('n', '<A-Right>',
                         ':execute "silent! tabmove " . (tabpagenr()+1)<CR>',
                         {noremap = true, silent = true})
+vim.keymap.set("n", "gc", function() require("tinygit").smartCommit() end)
+vim.keymap.set("n", "gp", function() require("tinygit").push() end)
 
 -- MacOS-specific settings
 if vim.fn.has('macunix') == 1 then
