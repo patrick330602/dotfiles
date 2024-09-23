c1171362952f16e1ff60dd60e383c4164d9b65f4
Jinming Wu, Patrick <me@patrickwu.space>
2024-09-23 14:37:20 +0800

    neovim: replace tabby with barbar

--- neovim/plugins/visual.lua
+++ neovim/plugins/visual.lua
@@ -26,16 +26,20 @@ return {
 		end,
 	},
 	{
-		"nanozuki/tabby.nvim",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("tabby").setup({
-				preset = "active_wins_at_tail",
-				option = {
-					nerdfont = true,
-					lualine_theme = "vscode",
-				},
-			})
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
 		end,
-	}
+		opts = {
+			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+	},
 }
