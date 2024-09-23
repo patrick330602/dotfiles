ed857a34f127d15cc8e8200200310f1c49859f95
Jinming Wu, Patrick <me@patrickwu.space>
2024-09-23 14:27:28 +0800

    neovim: small theme fix

--- neovim/plugins/visual.lua
+++ neovim/plugins/visual.lua
@@ -6,9 +6,11 @@ return {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.api.nvim_set_option_value("background", "dark", {})
+				vim.cmd("colorscheme vscode")
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option_value("background", "light", {})
+				vim.cmd("colorscheme vscode")
 			end,
 		},
 	},
