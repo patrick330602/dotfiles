c2c60f8cdbd9a8ff3d50d7fb2027f97b0ce96cd1
Patrick Wu <patrick.wu@fano.ai>
2024-10-09 18:33:22 +0800

    neovim: 10/09/2024 work update

--- neovim/plugins/games.lua
+++
@@ -1,2 +0,0 @@
-return { { "ThePrimeagen/vim-be-good" }
- }
---
+++ neovim/plugins/help.lua
@@ -0,0 +1,9 @@
+return {
+	{ "ThePrimeagen/vim-be-good" },
+	{ "tris203/precognition.nvim", opts = {} },
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+}
