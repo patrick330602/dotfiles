3664b09a803df7fae8df52144afc7cd5fb911d4e
Jinming Wu, Patrick <me@patrickwu.space>
2024-10-13 22:53:55 +0800

    neovim: reenable precognition and hardtime

--- neovim/plugins/help.lua
+++ neovim/plugins/help.lua
@@ -1,9 +1,10 @@
 return {
-	-- { "ThePrimeagen/vim-be-good" },
-	-- { "tris203/precognition.nvim", opts = {} },
-	-- {
-	-- 	"m4xshen/hardtime.nvim",
-	-- 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	-- 	opts = {},
-	-- },
+	{ "tris203/precognition.nvim", opts = {} },
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {
+			disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+		},
+	},
 }
