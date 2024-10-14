a46b3fcad16cc8bcfb72c31dac4fdfbeeee0f0b3
Patrick Wu <patrick.wu@fano.ai>
2024-10-14 17:51:08 +0800

    neovim: replace avante with copilotChat

--- neovim/plugins/ai.lua
+++ neovim/plugins/ai.lua
@@ -1,45 +1,14 @@
 return {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	lazy = false,
-	version = false,
-	opts = {
-		provider = "copilot",
-		auto_suggestions_provider = "copilot",
-		mappings = {
-			ask = "<leader>Ca",
-			edit = "<leader>Ce",
-			refresh = "<leader>Cr",
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
 		},
-	},
-	build = "make",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"zbirenbaum/copilot.lua",
-		{
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					use_absolute_path = true,
-				},
-			},
-		},
-		{
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			debug = false, -- Enable debugging
 		},
 	},
 }
