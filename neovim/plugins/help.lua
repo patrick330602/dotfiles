6257565d982bcc837eb7c50a5a472154815e8d66
Patrick Wu <patrick.wu@fano.ai>
2024-10-14 17:52:04 +0800

    neovim: use correct hardtime.nvim integration for precognition

--- neovim/plugins/help.lua
+++ neovim/plugins/help.lua
@@ -1,10 +1,28 @@
 return {
-	{ "tris203/precognition.nvim", opts = {} },
 	{
 		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "tris203/precognition.nvim" },
 		opts = {
-			disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+			callback = function(text)
+				vim.notify(text, vim.log.levels.WARN, { title = "hardtime" })
+				require("precognition").peek()
+			end,
+			disabled_filetypes = {
+				"qf",
+				"netrw",
+				"lazy",
+				"mason",
+				"oil",
+				"trouble",
+				"Telescope",
+				"TelescopePrompt",
+				"copilot-diff",
+				"copilot-system-prompt",
+				"copilot-user-selection",
+				"copilot-chat",
+				"NeoGitCommitMessage",
+				"NeoGitDiffView",
+			},
 		},
 	},
 }
