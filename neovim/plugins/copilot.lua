a43bdefdb34c0be71e8813ca214d825c9ecd6497
Jinming Wu, Patrick <me@patrickwu.space>
2024-10-28 18:21:17 +0800

    neovim: switch to copilot without using NodeJS

--- neovim/plugins/copilot.lua
+++ neovim/plugins/copilot.lua
@@ -1,15 +1,22 @@
 return {
-    { "github/copilot.vim" },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
-        dependencies = {
-            { "github/copilot.vim" }, -- or github/copilot.vim
-            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-        },
-        build = "make tiktoken",
-        opts = {
-            debug = true,
-        },
-    }
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken",
+		opts = {
+			debug = true,
+		},
+	},
 }
