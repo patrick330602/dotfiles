24a12ee245ee78f34506d4e90e4a79f397301b49
Jinming Wu, Patrick <me@patrickwu.space>
2025-01-08 18:11:33 +0800

    scripts: some further updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+echo "updating dependencies..."
+sh "$HOME/.dotfiles/scripts/deps.sh"
+
 # install starship config
 # if starship config already exists and not symlinked, back it up
 if test -f "$HOME/.config/starship.toml"; then
@@ -16,11 +20,11 @@ else
 fi
 
 if test -f "$HOME/.wezterm.lua"; then
-	echo "::Backing up Wezterm configuration..."
-	mv "$HOME/.wezterm.lua" "$HOME/.wezterm.lua.bak"
+    echo "::Backing up Wezterm configuration..."
+    mv "$HOME/.wezterm.lua" "$HOME/.wezterm.lua.bak"
 fi
 echo "::Installing Wezterm Configuration..."
-ln -s "$HOME/.dotfiles/.wezterm.lua" "$HOME/.wezterm.lua" 
+ln -s "$HOME/.dotfiles/.wezterm.lua" "$HOME/.wezterm.lua"
 
 if test -d "$HOME/.config/nvim" && test ! -L "$HOME/.config/nvim"; then
     echo "::Backing up existing nvim configuration..."
@@ -29,6 +33,8 @@ fi
 
 echo "::Installing nvim configuration..."
 ln -sfn "$HOME/.dotfiles/neovim" "$HOME/.config/nvim" && \
-nvim --headless "+Lazy! sync" +qa
+    nvim --headless "+Lazy! sync" +qa && \
+    nvim --headless "+TSUpdateSync" +qa && \
+    nvim --headless "+MasonToolsUpdateSync" +qa
 
 echo "::Done! Please restart your shell/terminal if needed."
