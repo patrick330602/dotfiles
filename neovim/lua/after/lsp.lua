8a2cb597c12eccf6e66ccbbaccb6e6369d7a0275
Jinming Wu, Patrick <me@patrickwu.space>
2025-04-16 15:38:47 +0800

    neovim: readd diagnostics symbols

--- neovim/lua/after/lsp.lua
+++ neovim/lua/after/lsp.lua
@@ -8,6 +8,14 @@ require("fidget").setup({
 	},
 })
 
+
+local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+
+for name, icon in pairs(symbols) do
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
+
 -- I like hint inlay
 vim.lsp.inlay_hint.enable()
 
